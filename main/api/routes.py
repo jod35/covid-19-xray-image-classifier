e200ffb389032459d0ac4c9cba54146ddbb41322
jod35 <jodestrevon@gmail.com>
2020-12-03 12:45:49 -0500

    successful file uploads

--- main/api/routes.py
+++ main/api/routes.py
@@ -1,25 +1,55 @@
-from flask import Blueprint,request,redirect,url_for,jsonify,make_response
+from flask import (
+    Blueprint,
+    request,
+    redirect,
+    url_for,
+    jsonify,
+    make_response,
+    current_app,
+    send_from_directory
+)
+
+
 from ..computations.predict import convert_to_array, predict_image_class
+from werkzeug.utils import secure_filename
 import base64
+import os
 
 
-api_bp=Blueprint('api',__name__)
+BASEDIR = os.path.dirname(os.path.realpath(__file__))
 
 
-@api_bp.route('/')
+api_bp = Blueprint("api", __name__)
+
+
+@api_bp.route("/")
 def hello():
-    return make_response(jsonify(
-        {"message":"Hello, welcome to the Xray classification API"}
-    ))
+    return make_response(
+        jsonify({"message": "Hello, welcome to the Xray classification API"})
+    )
 
-@api_bp.route('/predict',methods=['POST'])
+
+@api_bp.route("/predict", methods=["POST"])
 def predict_image():
-    file_name=request.form.get('filename')
+    file_name = request.form.get("filename")
     if request.files:
-        image=request.files['file_path']
+        image = request.files["file_path"]
 
-        array =convert_to_array(image)
-        image_class =predict_image_class(array)
-        print(f"\n{image_class}")
-    
-    return "<h1>Report</h1><p>Predictions:{}</p><p>Image Class :{}</p>".format(image_class['score'],image_class['class'])
+        file_name = secure_filename(image.filename)
+
+        image.save(os.path.join(current_app.config["UPLOADS_PATH"], file_name))
+
+        return "File saved"
+
+        # array =convert_to_array(image.filename)
+        # image_class =predict_image_class(array)
+        # print(f"\n{image_class}")
+
+        # print("\n\n{}".format(image))
+
+    return "<h1>Report</h1><p>Predictions:{}</p><p>Image Class :{}</p>"
+
+
+@app.config('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(current_app.config['UPLOADS_PATH'])
--- main/config.py
+++ main/config.py
@@ -3,11 +3,12 @@ import os
 
 BASEDIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-
 class Config:
     SECRET_KEY='fb0e79f78c817d9fa844a8127c87afc4402a41d64f275caac42f5f5ee69e5ddd'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
+    UPLOADS_PATH=os.path.join(BASEDIR,'uploads')
+    
+  
     
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI='sqlite:///'+BASEDIR+'/api.sqlite3'
