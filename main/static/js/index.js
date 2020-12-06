43cad9dcad3aa1b52d74ae9bdf7dae78ff98c9c4
jod35 <jodestrevon@gmail.com>
2020-12-06 11:27:22 -0500

    feat :display prediction data on frontend

--- main/static/js/index.js
+++ main/static/js/index.js
@@ -4,18 +4,36 @@
 Dropzone.options.filedrop = {
     init: function() {
         const loadingSection=document.querySelector('.loading');
-
+        const messageSection=document.querySelector('.msg');
+        const loadSpinner=document.querySelector('.load-img');
+        
         this.on("addedfile", function (file) {
+            
             loadingSection.style.visibility="visible";
           });
 
+        //when sending the file (uploading)
+        this.on("sending",function(file){
+            messageSection.innerText="Uploading ...";
+        });
+
+       
+
         this.on("success", function(file,response) { 
-            // alert(response.message);
-            loadingSection.innerText="Predicting Image Class";
+            messageSection.innerText="Predicting Image Class";
+
+            
 
             setTimeout(function(){
-                loadingSection.innerText=response.message;
-            },2000);
+                loadSpinner.style.visibility="hidden";
+                messageSection.innerText=response.message;
+            },3000);
+            
+
+            
+            this.removeAllFiles(true);
+
+           
         });
 
         
