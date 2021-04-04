20612d845d93191e6104e39d0c0aab7cc9772958
Ssali Jonathan <jodestrevin@gmail.com>
2021-04-04 17:20:45 -0400

    add pagination to file display

--- main/ui/routes.py
+++ main/ui/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint,render_template,request,url_for,send_from_directory,current_app
 from ..utils.database import db
-import os 
+import os
 from main.models.files import File
 from main.utils.database import db
 
@@ -16,8 +16,19 @@ def index():
 
 @ui_bp.route('/files')
 def show_records():
-    files=File.query.order_by(File.id.desc()).all()
-    return render_template('files.html',files=files)
+    page=request.args.get('page',1,type=int)
+
+
+    files=File.query.order_by(File.id.desc()).paginate(
+        page,6,False
+    )
+
+    next_url = url_for('ui.show_records', page=files.next_num) \
+        if files.has_next else None
+    prev_url = url_for('ui.show_records', page=files.prev_num) \
+        if files.has_prev else None
+
+    return render_template('files.html',files=files.items,next_url=next_url,prev_url=prev_url)
 
 @ui_bp.route('/upload_complete')
 def upload_complete():
@@ -33,3 +44,4 @@ def file_details(id):
     file_=File.query.get(id)
 
     return render_template('details.html',file_=file_)
+
